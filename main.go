89280fdba8d312ad16768ea551e02f520bc7c77a
henry <henry@pergegames.com.tw>
2022-07-08 12:15:41 +0800

    get/post

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Hello Gin.")
+	router := gin.Default()
+	router.GET("/ping", func(ctx *gin.Context) {
+		// gin.H: head of gin
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.POST("/ping:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		ctx.JSON(200, gin.H{
+			"id": id,
+		})
+	})
+	router.Run(":8080")
+}
